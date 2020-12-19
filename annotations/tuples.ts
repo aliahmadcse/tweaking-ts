bc48d32fd5b7d445fe266331a37ce0f80f7f97b3
aliahmadcse <aliahmadcse@gmail.com>
2020-12-19 14:26:27 +0500

    📦 NEW: tuples

--- annotations/tuples.ts
+++ annotations/tuples.ts
@@ -4,4 +4,10 @@ const drink = {
     sugar: 40,
 };
 
+type Drink = [string, boolean, number];
 
+const pepsi: Drink = ["brown", true, 40];
+
+const sprite: Drink = ["clear", true, 50];
+
+const tea: Drink = ["brown", false, 30];
