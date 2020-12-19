34cb7a1e08f044d1625fbfa03d263464678355e3
aliahmadcse <aliahmadcse@gmail.com>
2020-12-19 12:54:30 +0500

    📦 NEW: object annotation

---
+++ annotations/objects.ts
@@ -0,0 +1,19 @@
+const profile = {
+    name: "alex",
+    age: 20,
+    coords: {
+        lat: 20,
+        lng: 15,
+    },
+    setAge(age: number): void {
+        this.age = age;
+    },
+};
+
+const { age }: { age: number } = profile;
+
+const {
+    coords: { lat, lng },
+}: { coords: { lat: number; lng: number } } = profile;
+
+console.log(lat);
