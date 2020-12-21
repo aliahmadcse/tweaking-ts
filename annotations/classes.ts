e0ae7ec1553d5fb1052f2ae6c6fae43a4c8d8b86
aliahmadcse <aliahmadcse@gmail.com>
2020-12-21 21:59:34 +0500

    📦 NEW: classes

---
+++ annotations/classes.ts
@@ -0,0 +1,33 @@
+class Vehicle {
+    constructor(private _color: string) {}
+
+    protected honk(): void {
+        console.log("poop");
+    }
+
+    public get color(): string {
+        return this._color;
+    }
+
+    public set color(color: string) {
+        this._color = color;
+    }
+}
+
+class Car extends Vehicle {
+    constructor(color: string) {
+        super(color);
+    }
+
+    private drive(): void {
+        console.log("Room");
+    }
+
+    public startDrivingProcess(): void {
+        this.drive();
+        this.honk();
+    }
+}
+
+const vehicle = new Vehicle("red");
+console.log(vehicle.color);
