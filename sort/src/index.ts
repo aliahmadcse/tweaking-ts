95b8610ca3e74f808c32de507c3f416535b7076e
aliahmadcse <aliahmadcse@gmail.com>
2020-12-26 22:58:36 +0500

    📦 NEW: sort class init

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,10 @@
-console.log("ho cakn");
+class Sorter {
+    constructor(public collection: number[]) {}
+
+    sort(): void {}
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
+
