84bb632c66c092e0cfb6558313efe132984daaa9
aliahmadcse <aliahmadcse@gmail.com>
2020-12-28 22:39:42 +0500

    📦 NEW: CharacterCollection

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,9 +1,10 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
+// const numbersCollection = new NumbersCollection([-3, 2, 14, 1]);
 
-
-const numbersCollection = new NumbersCollection([-3, 2, 14, 1]);
-const sorter = new Sorter(numbersCollection);
+const charactersCollection = new CharactersCollection("saX");
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
