a06f3158bf2d9960e80af041b1548a8e01794d0d
aliahmadcse <aliahmadcse@gmail.com>
2020-12-29 22:18:37 +0500

    📦 NEW: sort project complete

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,10 +1,19 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 // const numbersCollection = new NumbersCollection([-3, 2, 14, 1]);
 
-const charactersCollection = new CharactersCollection("saX");
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
-console.log(charactersCollection.data);
+// const charactersCollection = new CharactersCollection("saX");
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(3);
+linkedList.add(-12);
+linkedList.add(11);
+linkedList.add(19);
+
+linkedList.sort();
+
+linkedList.print();
