f232bc48e2efad15479fca95e3decbb3a6036b45
aliahmadcse <aliahmadcse@gmail.com>
2021-01-07 19:10:23 +0500

    📦 NEW: stats file parse

--- stats/build/index.js
+++ stats/build/index.js
@@ -4,7 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = __importDefault(require("fs"));
-var matches = fs_1.default.readFileSync("./football.csv", {
+var matches = fs_1.default
+    .readFileSync("./football.csv", {
     encoding: "utf-8",
-});
-console.log(matches);
+})
+    .split("\n")
+    .map(function (row) { return row.split(","); });
+var manUnitedWon = 0;
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+    var match = matches_1[_i];
+    if ((match[1] == "Man United" && match[5] == MatchResult.HomeWin) ||
+        (match[2] == "Man United" && match[5] == MatchResult.AwayWin)) {
+        manUnitedWon++;
+    }
+}
+console.log(manUnitedWon);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,7 +1,28 @@
 import fs from "fs";
 
-const matches = fs.readFileSync("./football.csv", {
-    encoding: "utf-8",
-});
+const matches = fs
+    .readFileSync("./football.csv", {
+        encoding: "utf-8",
+    })
+    .split("\n")
+    .map((row: string): string[] => row.split(","));
 
-console.log(matches);
+let manUnitedWon = 0;
+
+enum MatchResult {
+    HomeWin = "H",
+    AwayWin = "A",
+    Draw = "D",
+}
+
+for (let match of matches) {
+    if (
+        (match[1] == "Man United" && match[5] == MatchResult.HomeWin) ||
+        (match[2] == "Man United" && match[5] == MatchResult.AwayWin)
+    ) {
+        manUnitedWon++;
+    }
+}
+
+
+console.log(manUnitedWon);
