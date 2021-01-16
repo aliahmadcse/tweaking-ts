ed148174e8ad091abd7b01824c102deb461be85a
aliahmadcse <aliahmadcse@gmail.com>
2021-01-16 21:43:38 +0500

    A nasty refactor"

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,19 +1,21 @@
-import {MatchReader} from "./MatchReader";
+import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./MatchResult";
+import { CsvFileReader } from "./CsvFileReader";
 
-const reader = new MatchReader("football.csv");
+const csvFileReader = new CsvFileReader("football.csv");
+const matchReader = new MatchReader(csvFileReader);
 
-reader.read();
+matchReader.load();
 
 let manUnitedWon = 0;
 
-for (let match of reader.data) {
-    if (
-        (match[1] == "Man United" && match[5] == MatchResult.HomeWin) ||
-        (match[2] == "Man United" && match[5] == MatchResult.AwayWin)
-    ) {
-        manUnitedWon++;
-    }
+for (let match of matchReader.matches) {
+  if (
+    (match[1] == "Man United" && match[5] == MatchResult.HomeWin) ||
+    (match[2] == "Man United" && match[5] == MatchResult.AwayWin)
+  ) {
+    manUnitedWon++;
+  }
 }
 
 console.log(`Man united has won ${manUnitedWon} games`);
